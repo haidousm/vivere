64bfaccb805e2fddd4a3a8172301a180c39be975
haidousm <haidous.m@gmail.com>
2021-11-22 15:00:17 +0200

    create month and day pickers

--- src/app/pages/diary/diary.page.ts
+++ src/app/pages/diary/diary.page.ts
@@ -6,10 +6,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./diary.page.scss'],
 })
 export class DiaryPage implements OnInit {
+  chosenMonth = 'November';
+  chosenYear = '2021';
+  days: Day[] = [
+    {
+      date: 1,
+      name: 'M',
+      selected: false,
+    },
+    {
+      date: 2,
+      name: 'T',
+      selected: true,
+    },
+    {
+      date: 3,
+      name: 'W',
+      selected: false,
+    },
+    {
+      date: 4,
+      name: 'T',
+      selected: false,
+    },
+    {
+      date: 5,
+      name: 'F',
+      selected: false,
+    },
+    {
+      date: 6,
+      name: 'S',
+      selected: false,
+    },
+    {
+      date: 7,
+      name: 'S',
+      selected: false,
+    },
+  ];
 
-  constructor() { }
+  slidesOptions = {
+    slidesPerView: 6.5,
+  };
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  previousMonth() {}
+
+  nextMonth() {}
+
+  selectDay(day: Day) {
+    this.days.forEach((d) => (d.selected = false));
+    day.selected = true;
   }
+}
 
+interface Day {
+  name: string;
+  date: number;
+  selected: boolean;
 }
