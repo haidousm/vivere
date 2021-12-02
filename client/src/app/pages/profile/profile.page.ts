0b98f3c54e6293cfbe1128a056f178d3f489b61f
haidousm <haidous.m@gmail.com>
2021-12-02 17:00:31 +0200

    impl update user data route

--- client/src/app/pages/profile/profile.page.ts
+++ client/src/app/pages/profile/profile.page.ts
@@ -10,19 +10,35 @@ import { User } from 'src/app/types/User';
 export class ProfilePage implements OnInit {
   user: User;
   isChanged: boolean;
-  constructor(private usersService: UsersService) {
+
+  userHeight: number;
+  userWeight: number;
+  userGoalWeight: number;
+  userGoalCalories: number;
+  constructor(private usersService: UsersService) {}
+  ngOnInit() {
     this.usersService.getCurrentUser().subscribe((user: User) => {
       this.user = user;
-      this.isChanged = false;
+      this.updateIsChanged();
     });
   }
 
-  ngOnInit() {}
-
   updateProfile() {
+    this.user.height = this.userHeight;
+    this.user.weight = this.userWeight;
+    this.user.goalWeight = this.userGoalWeight;
+    this.user.goalCalories = this.userGoalCalories;
     this.usersService.updateUser(this.user).subscribe((user: User) => {
       this.user = user;
-      this.isChanged = false;
+      this.updateIsChanged();
     });
+  }
+
+  updateIsChanged() {
+    this.isChanged =
+      this.userHeight === this.user.height &&
+      this.userWeight === this.user.weight &&
+      this.userGoalWeight === this.user.goalWeight &&
+      this.userGoalCalories === this.user.goalCalories;
   }
 }
