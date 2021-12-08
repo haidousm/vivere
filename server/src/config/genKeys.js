63cd6dfc8d3635f416438a574d65d1df4b49fb27
haidousm <haidous.m@gmail.com>
2021-12-08 21:17:09 +0200

    create priv and pub keys for jwt signing

---
+++ server/src/config/genKeys.js
@@ -0,0 +1,19 @@
+const crypto = require("crypto");
+const fs = require("fs");
+
+(() => {
+    const keyPair = crypto.generateKeyPairSync("rsa", {
+        modulusLength: 4096,
+        publicKeyEncoding: {
+            type: "pkcs1",
+            format: "pem",
+        },
+        privateKeyEncoding: {
+            type: "pkcs1",
+            format: "pem",
+        },
+    });
+
+    fs.writeFileSync(`${__dirname}/keys/public.pem`, keyPair.publicKey);
+    fs.writeFileSync(`${__dirname}/keys/private.pem`, keyPair.privateKey);
+})();
